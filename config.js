7595f48615e644142ddab08e7d63cbc29009a793
lrmn7 <lrmnid@gmail.com>
2023-07-24 22:47:55 +0700

    DISCORD BOT PERSONAL

---
+++ config.js
@@ -0,0 +1,134 @@
+module.exports = {
+  OWNER_IDS: ["742457036914294855"], // Bot owner ID's
+  SUPPORT_SERVER: "http://lrmn.is-a.dev/server-discord/", // Your bot support server
+  PREFIX_COMMANDS: {
+    ENABLED: true, // Enable/Disable prefix commands
+    DEFAULT_PREFIX: ".", // Default prefix for the bot
+  },
+  INTERACTIONS: {
+    SLASH: true, // Should the interactions be enabled
+    CONTEXT: true, // Should contexts be enabled
+    GLOBAL: true, // Should the interactions be registered globally
+    TEST_GUILD_ID: "1109793641058287626", // Guild ID where the interactions should be registered. [** Test you commands here first **]
+  },
+  EMBED_COLORS: {
+    BOT_EMBED: "#068ADD",
+    TRANSPARENT: "#36393F",
+    SUCCESS: "#00A56A",
+    ERROR: "#D61A3C",
+    WARNING: "#F7E919",
+  },
+  CACHE_SIZE: {
+    GUILDS: 100,
+    USERS: 10000,
+    MEMBERS: 10000,
+  },
+  MESSAGES: {
+    API_ERROR: "Error! Try again later or contact L RMN",
+  },
+
+  // PLUGINS
+
+  AUTOMOD: {
+    ENABLED: true,
+    LOG_EMBED: "#36393F",
+    DM_EMBED: "#36393F",
+  },
+
+  DASHBOARD: {
+    enabled: true, // enable or disable dashboard
+    baseURL: "http://localhost:8080", // base url
+    failureURL: "http://localhost:8080", // failure redirect url
+    port: "8080", // port to run the bot on
+  },
+
+  ECONOMY: {
+    ENABLED: true,
+    CURRENCY: "$",
+    DAILY_COINS: 100, // coins to be received by daily command
+    MIN_BEG_AMOUNT: 100, // minimum coins to be received when beg command is used
+    MAX_BEG_AMOUNT: 2500, // maximum coins to be received when beg command is used
+  },
+
+  MUSIC: {
+    ENABLED: true,
+    IDLE_TIME: 0, // Time in seconds before the bot disconnects from an idle voice channel
+    MAX_SEARCH_RESULTS: 5,
+    DEFAULT_SOURCE: "SC", // YT = Youtube, YTM = Youtube Music, SC = SoundCloud
+    // Add any number of lavalink nodes here
+    // Refer to https://github.com/freyacodes/Lavalink to host your own lavalink server
+    LAVALINK_NODES: [
+      {
+        host: "lavalink.devamop.in",
+        port: 80,
+        password: "DevamOP",
+        id:"Main",
+        secure: false
+      },
+    ],
+  },
+
+  GIVEAWAYS: {
+    ENABLED: true,
+    REACTION: "🎁",
+    START_EMBED: "#FF468A",
+    END_EMBED: "#FF468A",
+  },
+
+  IMAGE: {
+    ENABLED: true,
+    BASE_API: "https://strangeapi.fun/api",
+  },
+
+  INVITE: {
+    ENABLED: true,
+  },
+
+  MODERATION: {
+    ENABLED: true,
+    EMBED_COLORS: {
+      TIMEOUT: "#102027",
+      UNTIMEOUT: "#4B636E",
+      KICK: "#FF7961",
+      SOFTBAN: "#AF4448",
+      BAN: "#D32F2F",
+      UNBAN: "#00C853",
+      VMUTE: "#102027",
+      VUNMUTE: "#4B636E",
+      DEAFEN: "#102027",
+      UNDEAFEN: "#4B636E",
+      DISCONNECT: "RANDOM",
+      MOVE: "RANDOM",
+    },
+  },
+
+  PRESENCE: {
+    ENABLED: true, // Whether or not the bot should update its status
+    STATUS: "idle", // The bot's status [online, idle, dnd, invisible]
+    TYPE: "WATCHING", // Status type for the bot [PLAYING | LISTENING | WATCHING | COMPETING]
+    MESSAGE: "your dreams 💦", // Your bot status message
+  },
+
+  STATS: {
+    ENABLED: false,
+    XP_COOLDOWN: 5, // Cooldown in seconds between messages
+    DEFAULT_LVL_UP_MSG: "Congratulations {member:tag}, on leveling up **Level {level}** . Keep up the fantastic progress!",
+  },
+
+  SUGGESTIONS: {
+    ENABLED: true, // Should the suggestion system be enabled
+    EMOJI: {
+      UP_VOTE: "⬆️",
+      DOWN_VOTE: "⬇️",
+    },
+    DEFAULT_EMBED: "#4F545C",
+    APPROVED_EMBED: "#43B581",
+    DENIED_EMBED: "#F04747",
+  },
+
+  TICKET: {
+    ENABLED: true,
+    CREATE_EMBED: "#068ADD",
+    CLOSE_EMBED: "#068ADD",
+  },
+};
