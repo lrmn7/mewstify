a969d3842515a0e281f7cee3c09f4bd27e2139b0
lrmn7 <lrmnid@gmail.com>
2023-07-24 23:21:40 +0700

    update

--- config.js
+++ config.js
@@ -37,8 +37,8 @@ module.exports = {
 
   DASHBOARD: {
     enabled: true, // enable or disable dashboard
-    baseURL: "http://localhost:8080", // base url
-    failureURL: "http://localhost:8080", // failure redirect url
+    baseURL: "https://lrmnbot.hop.sh", // base url
+    failureURL: "https://lrmnbot.hop.sh", // failure redirect url
     port: "8080", // port to run the bot on
   },
 
